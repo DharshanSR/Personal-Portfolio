abc162582d5c198536f3606fb5c17259ed749aa7
DharshanSR <dharshanravindran8@gmail.com>
2024-09-19 23:04:13 +0530

    create a new ts file for store the technology svg icons

---
+++ src/data/techIcons.ts
@@ -0,0 +1,38 @@
+// languages
+import TypeScriptIcon from '@/public/assests/icons/typescript-original.svg';
+import JavaScriptIcon from '@/public/assests/icons/javascript-original.svg';
+import HTMLIcon from '@/public/assests/icons/html5-original.svg';
+import CSSIcon from '@/public/assests/icons/css3-original.svg';
+import pythonIcon from '@/public/assests/icons/python-original.svg';
+import javaIcon from '@/public/assests/icons/java-original.svg';
+import rIcon from '@/public/assests/icons/r-original.svg';
+
+// Frameworks
+import ReactIcon from '@/public/assests/icons/react-original.svg';
+import ReactNativeIcon from '@/public/assests/icons/react-original.svg';
+import NextJsIcon from '@/public/assests/icons/nextjs-original.svg';
+import NodeJsIcon from '@/public/assests/icons/nodejs-original.svg';
+import TailwindCssIcon from '@/public/assests/icons/tailwindcss-original.svg';
+import FramerMotionIcon from '@/public/assests/icons/framer-motion.png';
+
+// Version Control
+import GitIcon from '@/public/assests/icons/git-original.svg';
+import GitHubIcon from '@/public/assests/icons/github-original.svg';
+import GitLabIcon from '@/public/assests/icons/gitlab.png';
+
+// Backend & Databases
+import FirebaseIcon from '@/public/assests/icons/firebase-original.svg';
+import MongoDbIcon from '@/public/assests/icons/mongodb-original.svg';
+import PostgreSqlIcon from '@/public/assests/icons/postgresql-original.svg';
+import MySqlIcon from '@/public/assests/icons/mysql-original.svg';
+import ExpressIcon from '@/public/assests/icons/express-original.svg';
+import JsonIcon from '@/public/assests/icons/json-original.svg';
+import NpmIcon from '@/public/assests/icons/npm-original-wordmark.svg';
+import AppwriteIcon from '@/public/assests/icons/appwrite-original.svg';
+
+export const techIcons = {
+    'Next.js': NextJsIcon,
+    'TypeScript': TypeScriptIcon,
+    'Tailwind CSS': TailwindCssIcon,
+    // Add more technologies and icons as needed
+};
