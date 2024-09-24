d20f17a51dba0ec84e4f0c1fc622e118187163e5
DharshanSR <dharshanravindran8@gmail.com>
2024-09-24 23:08:02 +0530

    add the  padding

--- src/app/achievements/page.tsx
+++ src/app/achievements/page.tsx
@@ -44,7 +44,7 @@ const Achievements = () => {
 
     return (
         <>
-            <h2 id="achievements" className="text-3xl sm:text-5xl font-extrabold text-center mb-6 sm:mb-10 text-[#c3c3c0]">Achievements & Certificates</h2>
+            <h2 id="achievements" className="py-20 text-3xl sm:text-5xl font-extrabold text-center mb-6 sm:mb-10 text-[#c3c3c0]">Achievements & Certificates</h2>
             <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 sm:gap-8 px-4 sm:px-6 lg:px-8">
                 {certificates.map((certificate, index) => (
                     <div
