bc1530d814d8915c304d67c7032058a848422373
DharshanSR <dharshanravindran8@gmail.com>
2024-09-24 23:08:27 +0530

    fully change the ui style with animation

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -7,7 +7,8 @@ import { useRouter } from 'next/navigation';
 import Footer from '@/components/Footer';
 import Image from 'next/image';
 import { motion, useInView } from 'framer-motion';
-import {Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
+import { techIcons } from "@/data/techIcons";
 
 type ProjectDetailsPageProps = {
     params: {
@@ -26,10 +27,6 @@ const fadeIn = {
     animate: { opacity: 1 },
 };
 
-const hoverEffect = {
-    hover: { scale: 1.05, transition: { duration: 0.3 } },
-};
-
 const ProjectDetailsPage = ({ params }: ProjectDetailsPageProps) => {
     const router = useRouter();
     const project = projects.find((proj) => proj.id === params.projectId);
@@ -46,110 +43,151 @@ const ProjectDetailsPage = ({ params }: ProjectDetailsPageProps) => {
             animate="animate"
             exit={{ opacity: 0, transition: { duration: 0.5 } }}
         >
-            <Button>Back To Projects</Button>
+            <div className="container mx-auto px-6 py-12">
 
-            <div className="mx-auto px-6 py-12">
-                <motion.div
-                    className="grid grid-cols-1 md:grid-cols-2 gap-12"
+                <Button onClick={() => router.push('/projects')} className="mb-4">
+                    Back To Projects
+                </Button>
+
+                {/* Project Title */}
+                <motion.h1
+                    className="text-4xl font-extrabold text-center mb-8 text-blue-600"
                     variants={fadeInUp}
-                    transition={{ duration: 0.8 }}
+                    transition={{duration: 0.8}}
                 >
-                    {/* Project Image */}
-                    <motion.div
-                        className="relative shadow-lg rounded-lg overflow-hidden"
-                        variants={hoverEffect}
-                        whileHover="hover"
-                    >
-                        <Image
-                            src={project.image}
-                            alt={`Main image of ${project.title}`}
-                            className="w-full h-full object-cover"
-                            width={1200}
-                            height={600}
-                        />
-                    </motion.div>
+                    {project.title}
+                </motion.h1>
 
-                    {/* Project Details */}
-                    <motion.div
-                        className="flex flex-col justify-between"
-                        variants={fadeInUp}
-                        transition={{ delay: 0.1, duration: 0.8 }}
-                    >
-                        <div>
-                            <h1 className="text-4xl font-bold mb-4">{project.title}</h1>
-                            <p className="text-lg mb-6 text-justify">{project.detailedDescription}</p>
+                {/* Project Image */}
+                <motion.div
+                    className="relative shadow-lg rounded-lg overflow-hidden mb-8 w-[600px] mx-auto" // Set the width and center the image
+                    whileHover={{scale: 1.05, transition: {duration: 0.3}}}
+                    variants={fadeInUp}
+                    transition={{duration: 0.8}}
+                >
+                    <Image
+                        src={project.image}
+                        alt={`Main image of ${project.title}`}
+                        className="w-full h-auto object-cover"
+                        width={500}  // Adjust the width
+                        height={500}  // Adjust the height if necessary
+                    />
+                </motion.div>
 
-                            {/* Technologies Used (New UI Style) */}
-                            <div className="mb-6">
-                                <h2 className="text-2xl font-semibold mb-3">Technologies Used</h2>
-                                <div className="grid grid-cols-2 gap-4 bg-gray-100 dark:bg-gray-800 p-4 rounded-lg shadow-md">
-                                    {project.technologies.map((tech, idx) => (
-                                        <motion.div
-                                            key={idx}
-                                            className="flex items-center px-3 py-2 bg-white dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded-md shadow-sm cursor-pointer hover:bg-teal-100 dark:hover:bg-teal-800 transition"
-                                            whileHover={{ scale: 1.05 }}
-                                            transition={{ duration: 0.3 }}
-                                            style={{ cursor: 'pointer' }}  // Pointer cursor for interactivity
-                                        >
-                                            {tech}
-                                        </motion.div>
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
 
-                        {/* Action Buttons */}
-                        <motion.div
-                            className="mt-4 space-x-4"
-                            variants={fadeIn}
-                            transition={{ delay: 0.3, duration: 0.8 }}
-                        >
-                            {project.github && (
+                {/* Project Description */}
+                <motion.p
+                    className="text-lg mb-8 text-justify text-gray-700"
+                    variants={fadeInUp}
+                    transition={{duration: 0.8}}
+                >
+                    {project.detailedDescription}
+                </motion.p>
+
+                {/* Technologies Used Section */}
+                <motion.div
+                    className="mb-8"
+                    variants={fadeInUp}
+                    transition={{delay: 0.1, duration: 0.8}}
+                >
+                    <h2 className="text-xl font-semibold mb-2">Technologies Used</h2>
+
+                    <div className="flex flex-wrap justify-center gap-6">
+                        {project.technologies.map((tech, index) => (
+                            <motion.div
+                                key={index}
+                                className="relative p-6 bg-gray-100 rounded-full shadow-lg hover:bg-blue-500 hover:shadow-2xl transition-all duration-300 transform hover:scale-110 flex flex-col items-center"
+                                variants={fadeInUp}
+                                initial="initial"
+                                animate="animate"
+                                transition={{delay: 0.1 * index, duration: 0.5}}
+                            >
+                                {techIcons[tech] ? (
+                                    <div
+                                        className="bg-white rounded-full p-4 shadow-md hover:scale-110 transition-transform">
+                                        <Image
+                                            src={techIcons[tech]}
+                                            alt={`${tech} icon`}
+                                            width={40}
+                                            height={40}
+                                            className="mb-2"
+                                        />
+                                    </div>
+                                ) : (
+                                    <span className="text-center text-gray-500">{tech}</span>
+                                )}
+                                {/* On hover, change text color */}
+                                <span className="text-gray-700 text-sm font-semibold mt-2 group-hover:text-white">
+          {tech}
+        </span>
+                            </motion.div>
+                        ))}
+                    </div>
+                </motion.div>
+
+
+                {/* Action Buttons Section */}
+                <motion.div
+                    className="mb-12"
+                    initial={{opacity: 0, y: 50}}
+                    animate={{opacity: 1, y: 0}}
+                    transition={{delay: 0.3, duration: 0.8}}
+                >
+                    {/* GitHub Section */}
+                    {project.github && (
+                        <>
+                            <h2 className="text-xl font-semibold mb-4">GitHub Repository</h2>
+                            <div className="flex space-x-4 mb-8">
                                 <motion.a
                                     href={project.github}
                                     target="_blank"
                                     rel="noopener noreferrer"
-                                    className="inline-block bg-blue-600 text-white px-5 py-2 rounded-lg shadow-lg hover:bg-blue-500"
-                                    whileHover={{ scale: 1.05, transition: { duration: 0.3 } }}
+                                    className="inline-block bg-blue-600 text-white px-4 py-2 rounded-lg shadow-lg hover:bg-blue-500 transition duration-300"
+                                    whileHover={{scale: 1.05, transition: {duration: 0.3}}}
                                 >
                                     GitHub Repo
                                 </motion.a>
-                            )}
-                            {project.live && (
+                            </div>
+                        </>
+                    )}
+
+                    {/* Live Demo Section */}
+                    {project.live && (
+                        <>
+                            <h2 className="text-xl font-semibold mb-4">Live Demo</h2>
+                            <div className="flex space-x-4">
                                 <motion.a
                                     href={project.live}
                                     target="_blank"
                                     rel="noopener noreferrer"
-                                    className="inline-block bg-green-600 text-white px-5 py-2 rounded-lg shadow-lg hover:bg-green-500"
-                                    whileHover={{ scale: 1.05, transition: { duration: 0.3 } }}
+                                    className="inline-block bg-green-600 text-white px-4 py-2 rounded-lg shadow-lg hover:bg-green-500 transition duration-300"
+                                    whileHover={{scale: 1.05, transition: {duration: 0.3}}}
                                 >
                                     Live Demo
                                 </motion.a>
-                            )}
-                        </motion.div>
-                    </motion.div>
+                            </div>
+                        </>
+                    )}
                 </motion.div>
 
-                {/* More Images Section with In-View Animation */}
+
+                {/* More Images Section */}
                 {project.moreImages.length > 0 && (
                     <motion.div
                         ref={ref}
-                        initial={{ opacity: 0, y: 50 }}
-                        animate={isInView ? { opacity: 1, y: 0 } : {}}
-                        transition={{ delay: 0.3, duration: 0.8 }}
+                        initial={{opacity: 0, y: 50}}
+                        animate={isInView ? {opacity: 1, y: 0} : {}}
+                        transition={{delay: 0.3, duration: 0.8}}
                         className="mt-12"
                     >
                         <h2 className="text-3xl font-bold mb-4">More Images</h2>
 
-                        {/* Horizontal Scroll Gallery with Hover Effects */}
-                        <div className="relative flex overflow-x-auto space-x-6 pb-6">
+                        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
                             {project.moreImages.map((img, idx) => (
                                 <motion.div
                                     key={idx}
-                                    className="relative shadow-md rounded-lg overflow-hidden flex-shrink-0"
-                                    style={{ width: '400px', height: '250px' }}
-                                    whileHover={{ scale: 1.05 }}
-                                    transition={{ duration: 0.3 }}
+                                    className="relative shadow-lg rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-105"
+                                    whileHover={{scale: 1.05}}
                                 >
                                     <Image
                                         src={img}
@@ -163,9 +201,10 @@ const ProjectDetailsPage = ({ params }: ProjectDetailsPageProps) => {
                         </div>
                     </motion.div>
                 )}
+
             </div>
 
-            <Footer />
+            <Footer/>
         </motion.div>
     );
 };
