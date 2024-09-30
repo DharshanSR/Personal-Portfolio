509f7f31ec0131936680d434cfb66c2e9f0024e6
DharshanSR <dharshanravindran8@gmail.com>
2024-09-30 22:23:21 +0530

    add the new style for this fillter button

--- src/components/ProjectFilter.tsx
+++ src/components/ProjectFilter.tsx
@@ -17,9 +17,9 @@ const ProjectFilter = ({ categories, onFilterChange }: FilterProps) => {
     return (
         <motion.div
             className="mb-8"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.5 }}
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            transition={{duration: 0.5}}
         >
             <motion.div className="flex flex-wrap gap-2 items-center justify-center">
                 {categories.map((category) => (
@@ -27,20 +27,20 @@ const ProjectFilter = ({ categories, onFilterChange }: FilterProps) => {
                         key={category}
                         onClick={() => handleChange(category)}
                         className={`p-2 border rounded transition-colors duration-300 
-                            ${selectedCategory === category ? 'bg-[#071015] text-white' : 'bg-[#8f989b] text-[#071015] font-bold'} 
-                            hover:bg-[#6e6663] hover:text-white
-                        `}
-                        whileHover={{ scale: 1.05 }}
-                        whileTap={{ scale: 0.95 }}
-                        initial={{ opacity: 0, y: 20 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 0.3 }}
+                    ${selectedCategory === category ? 'bg-gradient-to-r from-blue-500 via-pink-500 to-red-500' : 'bg-white'} 
+                    text-gray-800 hover:text-white hover:bg-blue-600`}
+                        whileHover={{scale: 1.05}}
+                        whileTap={{scale: 0.95}}
+                        initial={{opacity: 0, y: 20}}
+                        animate={{opacity: 1, y: 0}}
+                        transition={{duration: 0.3}}
                     >
                         {category}
                     </motion.button>
                 ))}
             </motion.div>
         </motion.div>
+
     );
 };
 
