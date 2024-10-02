57f1eb38ec448999efe2b3757e9c3c5707fb9e81
DharshanSR <dharshanravindran8@gmail.com>
2024-10-02 23:48:39 +0530

    create a seperate page for machine learning service option

---
+++ src/app/service/machine-learning/page.tsx
@@ -0,0 +1,10 @@
+import DownFooter from "@/components/DownFooter";
+
+const MachineLearning = () => {
+    return (
+        <div>
+            <h1>Hi, this is the full stack page</h1>
+            <DownFooter/>
+        </div>)
+}
+export default MachineLearning
