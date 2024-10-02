e435996fbca73ea5cf7f133a410264ff3336a277
DharshanSR <dharshanravindran8@gmail.com>
2024-10-02 23:48:08 +0530

    create a seperate page for blog writting service option

---
+++ src/app/service/blog-writting/page.tsx
@@ -0,0 +1,10 @@
+import DownFooter from "@/components/DownFooter";
+
+const BlogWritting = () => {
+    return (
+        <div>
+            <h1>Hi, this is the full stack page</h1>
+            <DownFooter />
+        </div>    )
+}
+export default BlogWritting
