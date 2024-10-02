72f71f699f7c8c86c9d1a84869a96eade2c6f504
DharshanSR <dharshanravindran8@gmail.com>
2024-10-02 23:49:05 +0530

    create a seperate page for mobile app development service option

---
+++ src/app/service/mobile-app-development/page.tsx
@@ -0,0 +1,10 @@
+import DownFooter from "@/components/DownFooter";
+
+const MobileDevelopment = () => {
+    return (
+        <div>
+            <h1>Hi, this is the full stack page</h1>
+            <DownFooter/>
+        </div>)
+}
+export default MobileDevelopment
