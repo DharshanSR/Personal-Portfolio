85c9ec32119a2d157d675b2ad882f8a30dae09ae
DharshanSR <dharshanravindran8@gmail.com>
2024-10-03 23:54:47 +0530

    make when we click the button it will open to new window

--- src/components/BlogPreview.tsx
+++ src/components/BlogPreview.tsx
@@ -15,7 +15,7 @@ interface BlogPreviewProps {
 const BlogPreview: React.FC<BlogPreviewProps> = ({ title, description, date, link, image }) => {
     return (
         <div className="bg-[#c3c3c0] shadow-md rounded-lg overflow-hidden">
-            <Link href={link} className="block relative h-48">
+            <Link href={link} className="block relative h-48" target="_blank" rel="noopener noreferrer">
                 <Image src={image} alt={title} layout="fill" objectFit="cover" className="w-full h-full" />
             </Link>
             <div className="p-4 sm:p-6">
@@ -23,7 +23,7 @@ const BlogPreview: React.FC<BlogPreviewProps> = ({ title, description, date, lin
                 <p className="text-[#6e6663] mb-4 text-sm sm:text-base">{description}</p>
                 <span className="text-[#868b94] text-xs sm:text-sm font-bold">{date}</span>
                 <div className="mt-4">
-                    <Link href={link} className="inline-block bg-[#3c4b54] text-[#c3c3c0] px-3 py-2 rounded text-sm sm:text-base hover:bg-[#6e6663] transition">
+                    <Link href={link} target="_blank" rel="noopener noreferrer" className="inline-block bg-[#3c4b54] text-[#c3c3c0] px-3 py-2 rounded text-sm sm:text-base hover:bg-[#6e6663] transition">
                         Read More
                     </Link>
                 </div>
