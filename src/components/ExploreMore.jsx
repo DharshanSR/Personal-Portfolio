ee800f94fdafa657b21d69c23d1bb7efc466360d
DharshanSR <dharshanravindran8@gmail.com>
2024-10-10 23:04:07 +0530

    totally change some text and background color

--- src/components/ExploreMore.jsx
+++ src/components/ExploreMore.jsx
@@ -11,20 +11,20 @@ const ExploreMore = () => {
     return (
         <motion.div
             ref={ref}
-            className="bg-gray-100 py-20 text-center px-4"
+            className="bg-[#14141F] py-20 text-center px-4"
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: isInView ? 1 : 0, y: isInView ? 0 : 50 }} // Entry animation
             transition={{ duration: 0.5 }}
         >
-            <h4 className="text-3xl md:text-4xl font-bold mb-4 text-gray-800">
+            <h4 className="text-4xl md:text-4xl font-bold mb-2 text-cyan-500">
                 Do you want to see more about the blogs I wrote?
             </h4>
-            <p className="text-lg md:text-xl mb-6 max-w-6xl mx-auto text-gray-600">
+            <p className="text-lg md:text-xl mb-6 max-w-6xl mx-auto text-gray-200">
                 Explore a wide range of topics and insights from my writing journey.
             </p>
             <Link href="/#blogs"> {/* Use the hash to link to the ID */}
                 <motion.button
-                    className="bg-blue-600 text-white px-8 py-4 rounded-lg hover:bg-blue-500 transition-all duration-300 transform hover:scale-105"
+                    className="bg-cyan-400 text-white px-8 py-4 rounded-lg hover:bg-blue-500 transition-all duration-300 transform hover:scale-105"
                     whileHover={{ scale: 1.05 }} // Scale animation on hover
                 >
                     Explore Blogs
